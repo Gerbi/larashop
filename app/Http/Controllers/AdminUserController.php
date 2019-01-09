0c91334486b9931ba90f7971bf9285a0bb55a2ce
Gerbi <gerbi@protonmail.com>
2019-01-08 17:51:19 -0700

    Controller|Model|Migrations|Validations Login

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function store(Request $request)
+    {
+        //Validate the user
+        $request->validate([
+           'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //Log the user In
+
+        //Session message
+
+        //redirect
+
+    }
+
+}
