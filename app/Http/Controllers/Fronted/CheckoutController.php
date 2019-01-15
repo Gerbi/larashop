d8666dd3a4f4bd9e8de967f22d62c210d8ba16e5
Gerbi <gerbi@protonmail.com>
2019-01-14 23:52:35 -0700

    adding orders to db

--- app/Http/Controllers/Fronted/CheckoutController.php
+++ app/Http/Controllers/Fronted/CheckoutController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Fronted;
 
+use App\Order;
+use App\OrderItems;
+use Carbon\Carbon;
 use Cartalyst\Stripe\Laravel\Facades\Stripe;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
@@ -10,6 +13,11 @@ use App\Http\Controllers\Controller;
 
 class CheckoutController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('fronted.checkout.index');
@@ -34,10 +42,26 @@ class CheckoutController extends Controller
                 'metadata'    => [
                     'contents' => $contents,
                     'quantity' => Cart::instance('default')->count()
-
                 ]
-
             ]);
+
+            //insert into ordes table
+            $order = Order::create([
+                'user_id'   =>  auth()->user()->id,
+                'date'      =>  Carbon::now(),
+                'address'   =>  $request->address,
+                'status'    => 0
+            ]);
+            //insert into order items table
+            foreach (Cart::instance('default')->content() as $item)
+            {
+                OrderItems::create([
+                   'order_id'   =>  $order->id,
+                   'product_id' =>  $item->model->id,
+                   'quantity'   =>  $item->qty,
+                   'price'      =>  $item->price
+                ]);
+            }
 
             Cart::instance('default')->destroy();
 
--- resources/views/fronted/checkout/index.blade.php
+++ resources/views/fronted/checkout/index.blade.php
@@ -9,124 +9,130 @@
     <h2 class="mt-5"><i class="fa  fa-credit-card-alt"></i> Checkout</h2>
     <hr>
 
-    <div class="row">
+    @if (Cart::instance('default')->content < 0)
+        <h2>Empty cart please add some products</h2>
+        @else
+        <div class="row">
 
-        <div class="col-md-7">
+            <div class="col-md-7">
 
-            @if (session()->has('msg'))
-                <div class="alert alert-success">
-                    {{session()->get('msg')}}
-                </div>
-            @endif
+                @if (session()->has('msg'))
+                    <div class="alert alert-success">
+                        {{session()->get('msg')}}
+                    </div>
+                @endif
 
-            <h4>Billing Details</h4>
-            <form method="post" id="payment-form" action="{{ route('checkout') }}">
+                <h4>Billing Details</h4>
+                <form method="post" id="payment-form" action="{{ route('checkout') }}">
                     @csrf
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                    <div class="form-group">
+                        <label for="address">Address</label>
+                        <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St">
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="address">Address</label>
-                    <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-5">
-                        <label for="city">City</label>
-                        <input type="text" class="form-control" id="city" name="city" placeholder="City">
+                    <div class="form-row">
+                        <div class="form-group col-md-5">
+                            <label for="city">City</label>
+                            <input type="text" class="form-control" id="city" name="city" placeholder="City">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="province">Province</label>
+                            <input type="text" class="form-control" id="province" name="province" placeholder="Province">
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="postal">Postal</label>
+                            <input type="text" class="form-control" id="postal" name="postal" placeholder="Postal">
+                        </div>
                     </div>
-                    <div class="form-group col-md-4">
-                        <label for="province">Province</label>
-                        <input type="text" class="form-control" id="province" name="province" placeholder="Province">
+                    <div class="form-group">
+                        <label for="phone">Phone</label>
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
                     </div>
-                    <div class="form-group col-md-3">
-                        <label for="postal">Postal</label>
-                        <input type="text" class="form-control" id="postal" name="postal" placeholder="Postal">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="phone">Phone</label>
-                    <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
-                </div>
                     <hr>
                     <h5><i class="fa fa-credit-card"></i> Payment Details</h5>
-                <div class="form-group">
-                    <label for="name_card">Name on card</label>
-                    <input type="text" class="form-control" name="name_on_card" id="name_on_card" placeholder="Name on card">
-                </div>
-                <div class="form-group">
-                    <label for="card-element">
-                        Credit or debit card
-                    </label>
-                    <div id="card-element">
-                        <!-- A Stripe Element will be inserted here. -->
+                    <div class="form-group">
+                        <label for="name_card">Name on card</label>
+                        <input type="text" class="form-control" name="name_on_card" id="name_on_card" placeholder="Name on card">
+                    </div>
+                    <div class="form-group">
+                        <label for="card-element">
+                            Credit or debit card
+                        </label>
+                        <div id="card-element">
+                            <!-- A Stripe Element will be inserted here. -->
+                        </div>
+
+                        <!-- Used to display form errors. -->
+                        <div id="card-errors" role="alert"></div>
                     </div>
 
-                    <!-- Used to display form errors. -->
-                    <div id="card-errors" role="alert"></div>
-                </div>
+                    <button type="submit" class="btn btn-outline-info col-md-12">Complete Order</button>
+                </form>
 
-                <button type="submit" class="btn btn-outline-info col-md-12">Complete Order</button>
-            </form>
+            </div>
 
-        </div>
+            <div class="col-md-5">
 
-        <div class="col-md-5">
+                <h4>Your Order</h4>
 
-            <h4>Your Order</h4>
+                <table class="table your-order-table">
+                    <tr>
+                        <th>Image</th>
+                        <th>Details</th>
+                        <th>Qty</th>
+                    </tr>
 
-            <table class="table your-order-table">
-                <tr>
-                    <th>Image</th>
-                    <th>Details</th>
-                    <th>Qty</th>
-                </tr>
-
-                @foreach(Cart::instance('default')->content() as $item)
-                <tr>
-                    <td><img src="{{url('/uploads').'/'.$item->model->image}}" alt="" style="width: 4em"></td>
-                    <td>
-                        <strong>{{$item->model->name}}</strong>
-                        <br>
-                        {{$item->model->description}}<br>
-                        <span class="text-dark">{{$item->total()}}</span>
-                    </td>
-                    <td>
-                        <span class="badge badge-light">{{$item->qty}}</span>
-                    </td>
-                </tr>
+                    @foreach(Cart::instance('default')->content() as $item)
+                        <tr>
+                            <td><img src="{{url('/uploads').'/'.$item->model->image}}" alt="" style="width: 4em"></td>
+                            <td>
+                                <strong>{{$item->model->name}}</strong>
+                                <br>
+                                {{$item->model->description}}<br>
+                                <span class="text-dark">{{$item->total()}}</span>
+                            </td>
+                            <td>
+                                <span class="badge badge-light">{{$item->qty}}</span>
+                            </td>
+                        </tr>
                     @endforeach
 
-            </table>
+                </table>
 
-            <hr>
-            <table class="table your-order-table table-bordered">
-                <tr>
-                    <th colspan="2">Price Details</th>
-                </tr>
-                <tr>
-                    <td>Subtotal</td>
-                    <td>${{ Cart::subtotal()}}</td>
-                </tr>
-                <tr>
-                    <td>Tax</td>
-                    <td>${{ Cart::tax()}}</td>
-                </tr>
-                <tr>
-                    <th>Total</th>
-                    <th>${{ Cart::total()}}</th>
-                </tr>
+                <hr>
+                <table class="table your-order-table table-bordered">
+                    <tr>
+                        <th colspan="2">Price Details</th>
+                    </tr>
+                    <tr>
+                        <td>Subtotal</td>
+                        <td>${{ Cart::subtotal()}}</td>
+                    </tr>
+                    <tr>
+                        <td>Tax</td>
+                        <td>${{ Cart::tax()}}</td>
+                    </tr>
+                    <tr>
+                        <th>Total</th>
+                        <th>${{ Cart::total()}}</th>
+                    </tr>
 
-            </table>
+                </table>
 
+            </div>
         </div>
-    </div>
+    @endif
+
+
 
     <div class="mt-5"><hr></div>
 
